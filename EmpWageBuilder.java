45d35adf0b4813ace53c9c89545ad699202f1f62
pranay <pranaykawale500@gmail.com>
2021-10-21 13:09:09 +0530

    Added: Checking employee present or Absent

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,8 +1,21 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmpWageBuilder {
+
+	public static void empStatus(int num){
+		if(num == 1)
+			System.out.println("Employee Present");
+		else
+			System.out.println("Employee Absent");
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation Program");
+
+		Random randm = new Random();
+		int r = randm.nextInt(2);
+		empStatus(r);
 	}
 
 }
