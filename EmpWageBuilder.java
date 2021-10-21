9475c72af3af34fcf3ab75c17303aa62410ad3db
pranay <pranaykawale500@gmail.com>
2021-10-21 14:10:17 +0530

    Added:Wage_calculated_By_Switch_Case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,23 +4,23 @@ import java.util.Random;
 
 public class EmpWageBuilder {
 
-	public static void empStatus(int num){
-		int Wage_Per_HR = 20;
-		int Day_HRs = 8;
-		int PartTime_HRs = 8;
+	public static void empWageStatus(int num){
+		 int Wage_Per_HR = 20;
+	        int Day_HRs = 8;
+	        int PartTime_HRs = 8;
 
-		if(num == 1){
-			System.out.println("Employee Present");
-			System.out.println("DailyWage = " + Wage_Per_HR * Day_HRs);
-		}
-		else if(num == 2){
-			System.out.println("Employee Present");
-			System.out.println("DailyWage  Part Time = " + Wage_Per_HR * (Day_HRs + PartTime_HRs));
-		}
-
-		else{
-			System.out.println("Employee Absent");
-			System.out.println("DailyWage = " +0);
+	        switch (num){
+	            case 1:
+	                System.out.println("Employee Present");
+	                System.out.println("DailyWage = " + Wage_Per_HR * Day_HRs);
+	                break;
+	            case 2:
+	                System.out.println("Employee Present");
+	                System.out.println("DailyWage with Part Time = " + Wage_Per_HR * (Day_HRs + PartTime_HRs));
+	                break;
+	            case 0:
+	                System.out.println("Employee Absent");
+	                System.out.println("DailyWage = "+0);
 		}
 	}
 	public static void main(String[] args) {
@@ -28,7 +28,7 @@ public class EmpWageBuilder {
 
 		Random randm = new Random();
 		int r = randm.nextInt(3);
-		empStatus(r);
+		empWageStatus(r);
 	}
 
 }
