3c719fa8864c7659e74e59a0ffb1f868d326ea5b
pranay <pranaykawale500@gmail.com>
2021-10-21 16:52:04 +0530

    Added: Calculating employee Wage until condition reached

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,39 +6,43 @@ public class EmpWageBuilder {
 
 	public static void empWageForMonth(int num){
 		int Wage_Per_HR = 20;
-		int Day_HRs = 8;
-		int PartTime_HRs = 8;
-		int Working_Days = 20;
-		double dailywage = 0;
-		double monthlywage = 0;
+        int Day_HRs = 8;
+        int Part_Time_HRs = 4;
+        int Days = 20;
+        int Max_HRs_Per_Month = 100;
 
-		for (int i =0; i<Working_Days; i++){
-			switch (num){
-			case 1:
-				dailywage = Wage_Per_HR * Day_HRs * i;
+        int totalEmpHrs = 0;
+        int empHrs = 0;
+        int totalWorkingDays = 0;
+        double monthlywage = 0;
 
-				break;
-			case 2:
-				dailywage = Wage_Per_HR * (Day_HRs + PartTime_HRs) * i;
-				break;
-			case 0:
-				break;
-
-			}
-			monthlywage = monthlywage + dailywage;
-		}
-		System.out.println("Monthly Wage : " + monthlywage);
-
-
-	}
-
-	public static void main(String[] args) {
-
-		Random random = new Random();
-		int a = random.nextInt(3);
-		empWageForMonth(a);
-
-	}
+        while (totalEmpHrs <= Max_HRs_Per_Month && totalWorkingDays < Days){
+            totalWorkingDays++;
+            switch (num){
+                case 1:
+                    empHrs = Day_HRs;
+                    
+                    break;
+                case 2:
+                    empHrs = Part_Time_HRs;
+                    break;
+                case 0:
+                    empHrs = 0;
+                    break;
+            }
+            totalEmpHrs = totalEmpHrs + empHrs;
+        }
+        monthlywage = totalEmpHrs * Wage_Per_HR;
+        System.out.println("Monthly Wage : " + monthlywage);
 
 
+    }
+
+    public static void main(String[] args) {
+       
+        Random random = new Random();
+        int a = random.nextInt(3);
+        empWageForMonth(a);
+
+    }
 }
