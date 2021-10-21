c3706f2e15558dfbeb2eb5d63d6e21a6e1a5e814
pranay <pranaykawale500@gmail.com>
2021-10-21 16:32:15 +0530

    Added: Calculated monthly wage for 20 days

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,31 +4,41 @@ import java.util.Random;
 
 public class EmpWageBuilder {
 
-	public static void empWageStatus(int num){
-		 int Wage_Per_HR = 20;
-	        int Day_HRs = 8;
-	        int PartTime_HRs = 8;
+	public static void empWageForMonth(int num){
+		int Wage_Per_HR = 20;
+		int Day_HRs = 8;
+		int PartTime_HRs = 8;
+		int Working_Days = 20;
+		double dailywage = 0;
+		double monthlywage = 0;
 
-	        switch (num){
-	            case 1:
-	                System.out.println("Employee Present");
-	                System.out.println("DailyWage = " + Wage_Per_HR * Day_HRs);
-	                break;
-	            case 2:
-	                System.out.println("Employee Present");
-	                System.out.println("DailyWage with Part Time = " + Wage_Per_HR * (Day_HRs + PartTime_HRs));
-	                break;
-	            case 0:
-	                System.out.println("Employee Absent");
-	                System.out.println("DailyWage = "+0);
+		for (int i =0; i<Working_Days; i++){
+			switch (num){
+			case 1:
+				dailywage = Wage_Per_HR * Day_HRs * i;
+
+				break;
+			case 2:
+				dailywage = Wage_Per_HR * (Day_HRs + PartTime_HRs) * i;
+				break;
+			case 0:
+				break;
+
+			}
+			monthlywage = monthlywage + dailywage;
 		}
-	}
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Employee Wage Computation Program");
+		System.out.println("Monthly Wage : " + monthlywage);
 
-		Random randm = new Random();
-		int r = randm.nextInt(3);
-		empWageStatus(r);
+
 	}
+
+	public static void main(String[] args) {
+
+		Random random = new Random();
+		int a = random.nextInt(3);
+		empWageForMonth(a);
+
+	}
+
 
 }
