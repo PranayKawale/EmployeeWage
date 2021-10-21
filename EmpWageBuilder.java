a37797aaef7e1b9da45c88d6c758feda6ddb2e47
pranay <pranaykawale500@gmail.com>
2021-10-21 20:36:45 +0530

    Added : Wage for multiple comapanies calculated

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,47 +2,71 @@ package com.bridgelabz;
 
 import java.util.Random;
 
+
+
 public class EmpWageBuilder {
 
-	public static void empWageForMonth(int num){
-		int Wage_Per_HR = 20;
-        int Day_HRs = 8;
-        int Part_Time_Hrs = 4;
-        int Days = 20;
-        int Max_HRs_Per_Month = 100;
+	public static   int Full_Time_HRs = 8;
+	public static  int PartTime_HRs = 4;
+	double dailywage = 0;
+	double monthlywage = 0;
 
-        int totalEmpHrs = 0;
-        int empHrs = 0;
-        int totalWorkingDays = 0;
-        double monthlywage = 0;
+	int workingDays ;
+	int wagePerHour;
 
-        while (totalEmpHrs <= Max_HRs_Per_Month && totalWorkingDays < Days){
-            totalWorkingDays++;
-            switch (num){
-                case 1:
-                    empHrs = Day_HRs;
-                    
-                    break;
-                case 2:
-                    empHrs = Part_Time_Hrs;
-                    break;
-                case 0:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs = totalEmpHrs + empHrs;
-        }
-        monthlywage = totalEmpHrs * Wage_Per_HR;
-        System.out.println("Monthly Wage : " + monthlywage);
+	public  void setVariables(int x, int y) {
+		this.workingDays=x;
+		this.wagePerHour=y;
+	}
 
 
-    }
 
-    public static void main(String[] args) {
-       
-        Random random = new Random();
-        int a = random.nextInt(3);
-        empWageForMonth(a);
 
-    }
+	public  void wageForMultplCompny(){
+		Random random = new Random();
+		int empCheck = random.nextInt(3);
+
+
+		for (int i =0; i< workingDays; i++){
+
+			switch (empCheck){
+			case 1:
+				dailywage = wagePerHour * Full_Time_HRs * i;
+				break;
+			case 2:
+				dailywage = wagePerHour * (Full_Time_HRs + PartTime_HRs) * i;
+				break;
+			case 0:
+				break;
+
+			}
+			monthlywage = monthlywage + dailywage;
+		}
+		System.out.println( monthlywage);
+	}
+
+
+
+	public static void main(String[] args) {
+
+
+
+		EmpWageBuilder Tata = new EmpWageBuilder();
+		System.out.println("Monthley wage for Tata company is ");
+		Tata.setVariables(20, 50);
+		Tata.wageForMultplCompny();
+
+
+
+		EmpWageBuilder Hcl = new EmpWageBuilder();
+		System.out.println("Monthley wage for Hcl company is ");
+		Hcl.setVariables(25, 55);
+		Hcl.wageForMultplCompny();
+
+		EmpWageBuilder Infosys = new EmpWageBuilder();
+		System.out.println("Monthley wage for Infosys company is ");
+		Infosys.setVariables(30, 60);
+		Infosys.wageForMultplCompny();
+
+	}
 }
